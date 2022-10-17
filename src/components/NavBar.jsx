63a44424d4c142833c01d1bfa51f678a84ab4494
JhonyAL <jhonykl666@gmail.com>
2022-10-17 08:29:24 -0300

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar() {
+    return (
+        <div id="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Cadastrar</li>
+                <li>ASDSADAS</li>
+                <li>ASDFsaf</li>
+                <li>ASFDasf</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
